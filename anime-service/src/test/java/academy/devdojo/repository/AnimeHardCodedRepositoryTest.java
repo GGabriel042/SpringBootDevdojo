cc3bacffca837bb98e275df130309eea52e1ee0e
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-05-08 21:17:22 -0300

    Anime Repository Test

--- anime-service/src/test/java/academy/devdojo/repository/AnimeHardCodedRepositoryTest.java
+++ anime-service/src/test/java/academy/devdojo/repository/AnimeHardCodedRepositoryTest.java
@@ -1,9 +1,15 @@
 package academy.devdojo.repository;
 
+import academy.devdojo.domain.Anime;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +19,17 @@ class AnimeHardCodedRepositoryTest {
     private AnimeHardCodedRepository repository;
 
     @Mock
+    private AnimeData animeData;
+    private final List<Anime> animeList = new ArrayList<>();
 
+    @BeforeEach
+    void init() {
+        var steinsGate = Anime.builder().id(1L).name("Steins Gate").build();
+        var toradora = Anime.builder().id(2L).name("Toradora").build();
+        var pokemon = Anime.builder().id(3L).name("Pokemon").build();
+        animeList.addAll(List.of(steinsGate, toradora, pokemon));
+        BDDMockito.when(animeData.getAnimes()).thenReturn(animeList);
+    }
 
     @Test
     @DisplayName("findAll returns a list with all animes")
