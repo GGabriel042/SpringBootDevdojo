d6aa317aaf11aac5b9cf0c47dff21f79fdb1e557
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-05-18 18:57:20 -0300

    User Hardcore Repository Test

---
+++ user-service/src/test/java/academy/devdojo/repository/UserHardcoreRepositoryTest.java
@@ -0,0 +1,114 @@
+package academy.devdojo.repository;
+
+import academy.devdojo.commons.UserUtils;
+import academy.devdojo.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class UserHardcoreRepositoryTest {
+
+    @InjectMocks
+    private UserHardcoreRepository repository;
+    @InjectMocks
+    private UserUtils userUtils;
+
+    @Mock
+    private UserData userData;
+    private List<User> userList;
+
+    @BeforeEach
+    void init(){
+        userList = userUtils.newUserList();
+        BDDMockito.when(userData.getUsers()).thenReturn(userList);
+    }
+
+    @Test
+    @DisplayName("findAll returns a list with all animes")
+    @Order(1)
+    void findAll_ReturnsAllAnime_WhenSuccessful() {
+        var users = repository.findAll();
+        Assertions.assertThat(users).isNotNull().hasSameElementsAs(userList);
+    }
+
+    @Test
+    @DisplayName("findById returns a anime with given id")
+    @Order(2)
+    void findById_ReturnsAnime_WhenSuccessful() {
+        var expectedUser = userList.getFirst();
+        var user = repository.findById(expectedUser.getId());
+
+        Assertions.assertThat(user).isPresent().contains(expectedUser);
+    }
+
+    @Test
+    @DisplayName("findByName returns list with found object when name exists")
+    @Order(3)
+    void findByName_ReturnsFoundAnime_WhenNameIsFound() {
+        var expectedUser = userList.getFirst();
+        var user = repository.findByFirstName(expectedUser.getFirstName());
+
+        Assertions.assertThat(user).isNotNull().contains(expectedUser);
+    }
+
+    @Test
+    @DisplayName("findByName returns empty list when name is null")
+    @Order(4)
+    void findByName_ReturnsEmptyList_WhenIsNull() {
+        var user = repository.findByFirstName(null);
+
+        Assertions.assertThat(user).isNotNull().isEmpty();
+    }
+
+    @Test
+    @DisplayName("save creates a anime")
+    @Order(5)
+    void save_CreateAnime_WhenSuccessful() {
+        var id = userList.getLast().getId() + 1;
+        var userToBeSaved = User.builder().id(id).firstName("Rodolfo").lastName("Afonso").email("Rodolfonso@teste.com").build();
+
+        var userSaved = repository.saveUser(userToBeSaved);
+        Assertions.assertThat(userSaved).isEqualTo(userToBeSaved).hasNoNullFieldsOrProperties();
+
+        var userOptionalSaved = repository.findById(id);
+        Assertions.assertThat(userOptionalSaved).isPresent().contains(userToBeSaved);
+    }
+
+    @Test
+    @DisplayName("delete removes a anime")
+    @Order(6)
+    void delete_RemoveAnime_WhenSuccessful() {
+        var userToDelete = userList.getLast();
+        repository.deleteUser(userToDelete);
+
+        var users = repository.findAll();
+
+        Assertions.assertThat(users).isNotEmpty().doesNotContain(userToDelete);
+    }
+
+    @Test
+    @DisplayName("update updates a anime")
+    @Order(7)
+    void update_UpdateAnime_WhenSuccessful() {
+
+        var userToUpdate = userList.getFirst();
+        userToUpdate.setFirstName("Rodolfo");
+
+        repository.updateUser(userToUpdate);
+
+        var users = repository.findAll();
+        Assertions.assertThat(users).contains(userToUpdate);
+
+        var userOptionalUpdate = repository.findById(userToUpdate.getId());
+        Assertions.assertThat(userOptionalUpdate).isPresent();
+        Assertions.assertThat(userOptionalUpdate.get().getFirstName()).isEqualTo(userToUpdate.getFirstName());
+    }
+}
