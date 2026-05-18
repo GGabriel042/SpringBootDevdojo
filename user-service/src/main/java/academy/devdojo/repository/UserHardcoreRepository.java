0e103c878d3c873aa45059c5596f41cc65b43ab4
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-05-18 16:43:39 -0300

    User Hardcore Repository

---
+++ user-service/src/main/java/academy/devdojo/repository/UserHardcoreRepository.java
@@ -0,0 +1,42 @@
+package academy.devdojo.repository;
+
+import academy.devdojo.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserHardcoreRepository {
+
+    private final UserData userData;
+
+    public List<User> findAll() {
+        return userData.getUsers();
+    }
+
+    public Optional<User> findById (Long id) {
+        return userData.getUsers().stream().filter(user -> user.getId().equals(id)).findFirst();
+    }
+
+    public List<User> findByFirstName(String name) {
+        return userData.getUsers().stream().filter(user -> user.getFirstName().equalsIgnoreCase(name)).toList();
+    }
+
+    public User saveUser(User user) {
+        userData.getUsers().add(user);
+        return user;
+    }
+
+    public void deleteUser(User user) {
+        userData.getUsers().remove(user);
+    }
+
+    public void updateUser(User user) {
+        System.out.println("Repositorio recebeu "+ user.getFirstName());
+        deleteUser(user);
+        saveUser(user);
+    }
+}
