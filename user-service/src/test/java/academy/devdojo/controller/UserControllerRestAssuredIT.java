e9d885d32d1316991240d9b4aa5aa3946a35b46d
Gonzagam <canalblacktobio@gmail.com>
2026-06-02 20:31:33 -0300

    Exercise - Rest Assured Part 2

--- user-service/src/test/java/academy/devdojo/controller/UserControllerRestAssuredIT.java
+++ user-service/src/test/java/academy/devdojo/controller/UserControllerRestAssuredIT.java
@@ -4,22 +4,32 @@ import academy.devdojo.commons.FileUtils;
 import academy.devdojo.commons.UserUtils;
 import academy.devdojo.config.IntegrationTestConfig;
 import academy.devdojo.config.TestcontainersConfiguration;
+import academy.devdojo.repository.UserRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import net.javacrumbs.jsonunit.assertj.JsonAssertions;
 import net.javacrumbs.jsonunit.core.Option;
+import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -33,6 +43,8 @@ public class UserControllerRestAssuredIT extends IntegrationTestConfig {
     private FileUtils fileUtils;
     @LocalServerPort
     private int port;
+    @Autowired
+    private UserRepository repository;
 
     @BeforeEach
     void setUrl() {
@@ -71,84 +83,259 @@ public class UserControllerRestAssuredIT extends IntegrationTestConfig {
 
 
     @Test
-    @DisplayName("GET v1/users returns empty list when nothing is not found")
+    @DisplayName("GET v1/users?firstName=gabriel returns list with found object when first name exists")
+    @Sql(value = "/sql/user/init_three_user.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/user/clean_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Order(2)
-    void findAll_ReturnsEmptyList_WhenNothingIsNotFound() {
+    void findAll_ReturnsFoundUserInList_WhenFirstNameIsFound() {
 
-        var response = fileUtils.readResourceFile("user/get-users-empty-list-200.json");
+        var expectedResponse = fileUtils.readResourceFile("user/get-user-gabriel-first-name-200.json");
+        var firstName = "Gabriel";
 
-        RestAssured.given()
+        var response = RestAssured.given()
                 .contentType(ContentType.JSON).accept(ContentType.JSON)
                 .when()
+                .queryParam("firstName", firstName)
                 .get(URL)
                 .then()
                 .statusCode(HttpStatus.OK.value())
-                .body(Matchers.equalTo(response))
-                .log().all();
+                .log().all()
+                .extract().response().body().asString();;
+
+        JsonAssertions.assertThatJson(response)
+                .whenIgnoringPaths("[*].id")
+                .isEqualTo(expectedResponse);
     }
 
 
     @Test
-    @DisplayName("POST v1/users creates an user")
+    @DisplayName("GET v1/users?firstName=x returns empty list when first name is not found")
+    @Sql(value = "/sql/user/init_three_user.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/user/clean_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Order(3)
-    void save_CreatesUser_WhenSuccessful() throws Exception {
-        var request = fileUtils.readResourceFile("user/post-request-user-200.json");
-        var expectedResponse = fileUtils.readResourceFile("user/post-response-user-201.json");
+    void findAll_ReturnsEmptyList_WhenFirstNameIsNotFound() throws Exception {
+        var expectedResponse = fileUtils.readResourceFile("user/get-user-x-first-name-200.json");
+        var firstName = "x";
 
         var response = RestAssured.given()
                 .contentType(ContentType.JSON).accept(ContentType.JSON)
-                .body(request)
                 .when()
-                .post(URL)
+                .queryParam("firstName", firstName)
+                .get(URL)
                 .then()
-                .statusCode(HttpStatus.CREATED.value())
-                .log().all()
-                .extract().response().body().asString();
+                .statusCode(HttpStatus.OK.value())
+                .body(Matchers.equalTo(expectedResponse))
+                .log().all();
 
         JsonAssertions.assertThatJson(response)
-                .node("id")
-                .asNumber()
-                .isPositive();
+                .whenIgnoringPaths("[*].id")
+                .isEqualTo(expectedResponse);
+    }
+
+
+    @Test
+    @DisplayName("GET v1/users/1 returns an user with given id")
+    @Sql(value = "/sql/user/init_three_user.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/user/clean_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @Order(4)
+    void findById_ReturnsUserById_WhenSuccessful() throws Exception {
+        var expectedResponse = fileUtils.readResourceFile("user/get-user-by-id-200.json");
+        var users = repository.findByFirstNameIgnoreCase("Gabriel");
+        Assertions.assertThat(users).hasSize(1);
+
+        var response = RestAssured.given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .when()
+                .pathParam("id", users.getFirst().getId())
+                .get(URL+"/{id}")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body(Matchers.equalTo(expectedResponse))
+                .log().all();
 
         JsonAssertions.assertThatJson(response)
                 .whenIgnoringPaths("id")
                 .isEqualTo(expectedResponse);
     }
 
+    @Test
+    @DisplayName("GET v1/users/99 throws NotFound 404 when user is not found")
+    @Order(5)
+    void findById_ThrowsNotFound_WhenUserIsNotFound() throws Exception {
+        var response = fileUtils.readResourceFile("user/get-user-by-id-404.json");
+
+        var id = 99L;
+
+        mockMvc.perform(MockMvcRequestBuilders.get(URL + "/{id}", id))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    @Test
+    @DisplayName("POST v1/users creates an user")
+    @Order(6)
+    void save_CreatesUser_WhenSuccessful() throws Exception {
+        var request = fileUtils.readResourceFile("user/post-request-user-200.json");
+        var response = fileUtils.readResourceFile("user/post-response-user-201.json");
+        var userSaved = userUtils.newUserSaved();
+
+        BDDMockito.when(repository.save(ArgumentMatchers.any())).thenReturn(userSaved);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post(URL)
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
+
+    @Test
+    @DisplayName("DELETE v1/users/1 removes an user")
+    @Order(7)
+    void delete_RemoveUser_WhenSuccessful() throws Exception {
+        var id = userList.getFirst().getId();
+
+        var foundUser = userList.stream().filter(user -> user.getId().equals(id)).findFirst();
+        BDDMockito.when(repository.findById(id)).thenReturn(foundUser);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete(URL + "/{id}", id))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    @DisplayName("DELETE v1/users/99 throws NotFound when user is not found")
+    @Order(8)
+    void delete_ThrowsNotFound_WhenUserIsNotFound() throws Exception {
+        var response = fileUtils.readResourceFile("user/delete-user-by-id-404.json");
+        var id = 99L;
+
+        mockMvc.perform(MockMvcRequestBuilders.delete(URL + "/{id}", id))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+
+    }
+
+    @Test
+    @DisplayName("PUT v1/users updates an user")
+    @Order(9)
+    void update_UpdatesUser_WhenSuccessful() throws Exception {
+        var request = fileUtils.readResourceFile("user/put-request-user-200.json");
+        var id = 1L;
+        var foundUser = userList.stream().filter(user -> user.getId().equals(id)).findFirst();
+        BDDMockito.when(repository.findById(id)).thenReturn(foundUser);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put(URL)
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    @DisplayName("PUT v1/users throws NotFound when user is not found")
+    @Order(10)
+    void update_ThrowsNotFound_WhenUserIsNotFound() throws Exception {
+        var request = fileUtils.readResourceFile("user/put-request-user-404.json");
+        var response = fileUtils.readResourceFile("user/put-user-by-id-404.json");
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put(URL)
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().json(response));
+    }
 
     @ParameterizedTest
     @MethodSource("postUserBadRequestSource")
     @DisplayName("POST v1/users returns bad request when fields are invalid")
-    @Order(4)
-    void save_ReturnsBadRequest_WhenFieldsAreInvalid(String requestFile, String responseFile) throws Exception {
-        var request = fileUtils.readResourceFile("user/%s".formatted(requestFile));
-        var expectedResponse = fileUtils.readResourceFile("user/%s".formatted(responseFile));
+    @Order(11)
+    void save_ReturnsBadRequest_WhenFieldsAreInvalid(String fileName, List<String> errors) throws Exception {
+        var request = fileUtils.readResourceFile("user/%s".formatted(fileName));
 
+        var mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .post(URL)
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn();
 
-        var response = RestAssured.given()
-                .contentType(ContentType.JSON).accept(ContentType.JSON)
-                .body(request)
-                .when()
-                .post(URL)
-                .then()
-                .statusCode(HttpStatus.BAD_REQUEST.value())
-                .log().all()
-                .extract().response().body().asString();
+        var resolvedException = mvcResult.getResolvedException();
 
-        JsonAssertions.assertThatJson(response)
-                .whenIgnoringPaths("timestamp")
-                .when(Option.IGNORING_ARRAY_ORDER)
-                .isEqualTo(expectedResponse);
+        org.assertj.core.api.Assertions.assertThat(resolvedException).isNotNull();
 
+        org.assertj.core.api.Assertions.assertThat(resolvedException.getMessage()).contains(errors);
+    }
+
+    @ParameterizedTest
+    @MethodSource("putUserBadRequestSource")
+    @DisplayName("PUT v1/users returns bad request when fields are invalid")
+    @Order(12)
+    void update_ReturnsBadRequest_WhenFieldsAreInvalid(String fileName, List<String> errors) throws Exception {
+        var request = fileUtils.readResourceFile("user/%s".formatted(fileName));
+
+        var mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .put(URL)
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andReturn();
+
+        var resolvedException = mvcResult.getResolvedException();
+
+        org.assertj.core.api.Assertions.assertThat(resolvedException).isNotNull();
+
+        Assertions.assertThat(resolvedException.getMessage()).contains(errors);
+    }
+
+    private static Stream<Arguments> putUserBadRequestSource() {
+        var allRequiredErrors = allRequiredErrors();
+        allRequiredErrors.add("The field 'id' cannot be null");
+        var emailInvalidError = invalidEmailErrors();
+
+        return Stream.of(
+                Arguments.of("put-request-user-empty-fields-400.json", allRequiredErrors),
+                Arguments.of("put-request-user-blank-fields-400.json", allRequiredErrors),
+                Arguments.of("put-request-user-invalid-email-400.json", emailInvalidError)
+        );
     }
 
     private static Stream<Arguments> postUserBadRequestSource() {
+        var allRequiredErrors = allRequiredErrors();
+        var emailInvalidError = invalidEmailErrors();
 
         return Stream.of(
-                Arguments.of("post-request-user-empty-fields-400.json", "post-response-user-empty-fields-400.json"),
-                Arguments.of("post-request-user-null-fields-400.json", "post-response-user-null-fields-400.json"),
-                Arguments.of("post-request-user-blank-fields-400.json", "post-response-user-blank-fields-400.json")
+                Arguments.of("post-request-user-empty-fields-400.json", allRequiredErrors),
+                Arguments.of("post-request-user-blank-fields-400.json", allRequiredErrors),
+                Arguments.of("post-request-user-invalid-email-400.json", emailInvalidError)
         );
+    }
+
+    private static List<String> invalidEmailErrors() {
+        var emailInvalidError = "The e-mail is not valid";
+        return List.of(emailInvalidError);
+    }
+
+    private static List<String> allRequiredErrors() {
+        var firstNameRequiredError = "The field 'firstName' is required";
+        var lastNameRequiredError = "The field 'lastName' is required";
+        var emailRequiredError = "The field 'email' is required";
+
+        return new ArrayList<>(List.of(firstNameRequiredError, lastNameRequiredError, emailRequiredError));
     }
 
 
