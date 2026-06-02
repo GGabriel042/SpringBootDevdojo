2ff1c4c06ba0439b5258366d3c0f8ed99fa78365
Gonzagam <canalblacktobio@gmail.com>
2026-06-02 18:15:12 -0300

    Transactional in Integration Test

--- user-service/src/test/java/academy/devdojo/controller/ProfileControllerIT.java
+++ user-service/src/test/java/academy/devdojo/controller/ProfileControllerIT.java
@@ -32,7 +32,6 @@ import static org.springframework.http.HttpMethod.POST;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Transactional
 @Import(TestcontainersConfiguration.class)
 public class ProfileControllerIT extends IntegrationTestConfig {
     private static final String URL = "/v1/profiles";
@@ -45,7 +44,8 @@ public class ProfileControllerIT extends IntegrationTestConfig {
 
     @Test
     @DisplayName("GET v1/profiles returns a list with all profiles")
-    @Sql(value = "/sql/init_two_profiles.sql")
+    @Sql(value = "/sql/init_two_profiles.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/clean_profiles.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Order(1)
     void findAll_ReturnsAllProfiles_WhenSuccessful() throws Exception {
         var typeReference = new ParameterizedTypeReference<List<ProfileGetResponse>>(){};
@@ -53,7 +53,7 @@ public class ProfileControllerIT extends IntegrationTestConfig {
 
         assertThat(responseEntity).isNotNull();
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isNotNull().doesNotContainNull();
+        assertThat(responseEntity.getBody()).isNotEmpty().doesNotContainNull();
 
         responseEntity.getBody()
                 .forEach(profileGetResponse -> assertThat(profileGetResponse).hasNoNullFieldsOrProperties());
