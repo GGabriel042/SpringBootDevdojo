d46bb0226e378eac1bbd6c07ee8d1b49114d9445
Gonzagam <canalblacktobio@gmail.com>
2026-06-01 22:50:38 -0300

    Integration Test part 3

--- user-service/src/test/java/academy/devdojo/controller/ProfileControllerIT.java
+++ user-service/src/test/java/academy/devdojo/controller/ProfileControllerIT.java
@@ -2,23 +2,37 @@ package academy.devdojo.controller;
 
 import academy.devdojo.commons.FileUtils;
 import academy.devdojo.commons.ProfileUtils;
+import academy.devdojo.config.TestcontainersConfiguration;
 import academy.devdojo.response.ProfileGetResponse;
 import academy.devdojo.response.ProfilePostResponse;
+import jakarta.transaction.Transactional;
+import net.javacrumbs.jsonunit.assertj.JsonAssertions;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.springframework.http.HttpMethod.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Transactional
+@Import(TestcontainersConfiguration.class)
 public class ProfileControllerIT {
     private static final String URL = "/v1/profiles";
     @Autowired
@@ -48,7 +62,7 @@ public class ProfileControllerIT {
     @Test
     @DisplayName("GET v1/profiles returns empty list when nothing is not found")
     @Order(2)
-    void findAll_ReturnsEmptyList_WhenNothingIsNotFound() throws Exception{
+    void findAll_ReturnsEmptyList_WhenNothingIsNotFound() {
         var typeReference = new ParameterizedTypeReference<List<ProfileGetResponse>>(){};
         var responseEntity = testRestTemplate.exchange(URL, GET, null, typeReference);
 
@@ -70,6 +84,36 @@ public class ProfileControllerIT {
         assertThat(responseEntity).isNotNull();
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         assertThat(responseEntity.getBody()).isNotNull().hasNoNullFieldsOrProperties();
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("postProfileBadRequestSource")
+    @DisplayName("POST v1/profiles returns bad request when fields are invalid")
+    @Order(4)
+    void save_ReturnsBadRequest_WhenFieldsAreInvalid(String requestFile, String responseFile) throws Exception {
+        var request = fileUtils.readResourceFile("profile/%s".formatted(requestFile));
+        var expectedResponse = fileUtils.readResourceFile("profile/%s".formatted(responseFile));
+
+        var profileEntity = buildHttpEntity(request);
+        var responseEntity = testRestTemplate.exchange(URL, POST, profileEntity, String.class);
+
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+
+        JsonAssertions.assertThatJson(responseEntity.getBody())
+                .whenIgnoringPaths("timestamp")
+                .isEqualTo(expectedResponse);
+
+    }
+
+    private static Stream<Arguments> postProfileBadRequestSource() {
+
+        return Stream.of(
+                Arguments.of("post-request-profile-empty-fields-400.json", "post-response-profile-empty-fields-400.json"),
+                Arguments.of("post-request-profile-null-fields-400.json", "post-response-profile-null-fields-400.json"),
+                Arguments.of("post-request-profile-blank-fields-400.json", "post-response-profile-blank-fields-400.json")
+        );
     }
 
 
