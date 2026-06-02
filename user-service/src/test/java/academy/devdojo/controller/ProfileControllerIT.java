ae807639ea1f3c96dfd6b618709b2b2fafb0d78d
Gonzagam <canalblacktobio@gmail.com>
2026-06-01 21:00:39 -0300

    Integration Test part 2

--- user-service/src/test/java/academy/devdojo/controller/ProfileControllerIT.java
+++ user-service/src/test/java/academy/devdojo/controller/ProfileControllerIT.java
@@ -1,24 +1,32 @@
 package academy.devdojo.controller;
 
+import academy.devdojo.commons.FileUtils;
+import academy.devdojo.commons.ProfileUtils;
 import academy.devdojo.response.ProfileGetResponse;
-import org.assertj.core.api.Assertions;
+import academy.devdojo.response.ProfilePostResponse;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.http.HttpMethod.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProfileControllerIT {
     private static final String URL = "/v1/profiles";
     @Autowired
-    private TestRestTemplate restTemplate;
+    private TestRestTemplate testRestTemplate;
+    @Autowired
+    private ProfileUtils profileUtils;
+    @Autowired
+    private FileUtils fileUtils;
 
     @Test
     @DisplayName("GET v1/profiles returns a list with all profiles")
@@ -26,13 +34,49 @@ public class ProfileControllerIT {
     @Order(1)
     void findAll_ReturnsAllProfiles_WhenSuccessful() throws Exception {
         var typeReference = new ParameterizedTypeReference<List<ProfileGetResponse>>(){};
-        var responseEntity = restTemplate.exchange(URL, HttpMethod.GET, null, typeReference);
+        var responseEntity = testRestTemplate.exchange(URL, GET, null, typeReference);
 
-        Assertions.assertThat(responseEntity).isNotNull();
-        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Assertions.assertThat(responseEntity.getBody()).isNotNull().doesNotContainNull();
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isNotNull().doesNotContainNull();
 
         responseEntity.getBody()
-                .forEach(profileGetResponse -> Assertions.assertThat(profileGetResponse).hasNoNullFieldsOrProperties());
+                .forEach(profileGetResponse -> assertThat(profileGetResponse).hasNoNullFieldsOrProperties());
+    }
+
+
+    @Test
+    @DisplayName("GET v1/profiles returns empty list when nothing is not found")
+    @Order(2)
+    void findAll_ReturnsEmptyList_WhenNothingIsNotFound() throws Exception{
+        var typeReference = new ParameterizedTypeReference<List<ProfileGetResponse>>(){};
+        var responseEntity = testRestTemplate.exchange(URL, GET, null, typeReference);
+
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isNotNull().isEmpty();
+
+    }
+
+
+    @Test
+    @DisplayName("POST v1/profiles creates an profile")
+    @Order(3)
+    void save_CreatesProfile_WhenSuccessful() throws Exception{
+        var request = fileUtils.readResourceFile("profile/post-request-profile-200.json");
+        var profileEntity = buildHttpEntity(request);
+        var responseEntity = testRestTemplate.exchange(URL, POST, profileEntity, ProfilePostResponse.class);
+
+        assertThat(responseEntity).isNotNull();
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(responseEntity.getBody()).isNotNull().hasNoNullFieldsOrProperties();
+    }
+
+
+
+    private static HttpEntity<String> buildHttpEntity(String request) {
+        var httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity<>(request, httpHeaders);
     }
 }
