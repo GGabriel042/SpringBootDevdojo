116ed5972764f5812db408d38df656875f52c8d0
Gonzagam <canalblacktobio@gmail.com>
2026-06-01 23:45:06 -0300

    Testcontainers Repository

--- user-service/src/test/resources/sql/init_one_user.sql
+++ user-service/src/test/resources/sql/init_one_user.sql
@@ -1,1 +1,1 @@
-insert into "user" (email,first_name,last_name) values ('Rodolfonso@teste.com','Rodolfo','Afonso')
+insert into user (email,first_name,last_name) values ('Rodolfonso@teste.com','Rodolfo','Afonso')
