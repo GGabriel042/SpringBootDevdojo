3b7d5a741831249d7668231b6004709fba0153f8
Gonzagam <canalblacktobio@gmail.com>
2026-06-05 04:11:54 -0300

    Spring Security Authorization Part 12

---
+++ user-service/src/test/java/academy/devdojo/commons/Constants.java
@@ -0,0 +1,7 @@
+package academy.devdojo.commons;
+
+public class Constants {
+    public static final String BASE_URI = "http://localhost:";
+    public static final String REGULAR_USERNAME = "MestreKame@teste.com";
+    public static final String PASSWORD = "test";
+}
