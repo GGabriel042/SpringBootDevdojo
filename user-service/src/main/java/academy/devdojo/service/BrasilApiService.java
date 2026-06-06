efbd4a4314bdb969d161b9a28ae37625c4c12ea6
Gonzagam <canalblacktobio@gmail.com>
2026-06-05 21:42:14 -0300

    Rest Client - Part 2

--- user-service/src/main/java/academy/devdojo/service/BrasilApiService.java
+++ user-service/src/main/java/academy/devdojo/service/BrasilApiService.java
@@ -1,9 +1,12 @@
 package academy.devdojo.service;
 
 import academy.devdojo.config.BrasilApiConfigurationProperties;
+import academy.devdojo.exception.NotFoundException;
 import academy.devdojo.response.CepGetResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -13,12 +16,18 @@ public class BrasilApiService {
 
     private final RestClient.Builder brasilApiClient;
     private final BrasilApiConfigurationProperties brasilApiConfigurationProperties;
+    private final ObjectMapper mapper;
 
     public CepGetResponse findCep(String cep) {
         return brasilApiClient.build()
                 .get()
                 .uri(brasilApiConfigurationProperties.cepUri(), cep)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, (request, response) -> {
+                    String body = new String(response.getBody().readAllBytes());
+                    var cepErrorResponse = mapper.readValue(body, CepGetResponse.class);
+                    throw new NotFoundException(cepErrorResponse.toString());
+                })
                 .body(CepGetResponse.class);
     }
 }
